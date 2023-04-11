b7f14a6ab8e7b723f699325b333c46d6367e574a
Stupid-wangnz <1171100402@qq.com>
2023-04-11 13:22:43 +0000

    MLPMixer

--- CNN/ResNet.py
+++ CNN/ResNet.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
-from zmq import Flag
 from typing import Optional
 import torch.optim as optim
 
@@ -19,14 +18,14 @@ transform = transforms.Compose(
 [transforms.ToTensor(),
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-batch_size = 4
+batch_size = 128
 
-trainset = torchvision.datasets.CIFAR10(root='E:\DeepLearning\CNN\data', train=True,
+trainset = torchvision.datasets.CIFAR10(root='/codes/DeepLearning/DL/CNN/data', train=True,
                                         download=False, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                           shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(root='E:\DeepLearning\CNN\data', train=False,
+testset = torchvision.datasets.CIFAR10(root='/codes/DeepLearning/DL/CNN/data', train=False,
                                        download=False, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                          shuffle=False, num_workers=2)
@@ -37,23 +36,22 @@ classes = ('plane', 'car', 'bird', 'cat',
 class BasicBlock(nn.Module):
     def __init__(self, in_channels, out_channels, stride, downsample : Optional[nn.Module] = None) :
         super().__init__()
-        self.relu = nn.ReLU()
+        self.relu = nn.ReLU(inplace=True)
 
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(out_channels)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(out_channels)
         self.downsample = downsample
 
     def forward(self, x):
         _x = x
-        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.relu(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
-
         if self.downsample is not None:
             _x = self.downsample(x)
 
-        out = self.relu(_x + out)
+        out = F.relu(_x + out)
         
         return out
 
@@ -62,45 +60,74 @@ class BasicBlock(nn.Module):
 class ResNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False) # 3 32 32 -> 64 16 16
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False) # 3 32 32 -> 64 32 32
         self.bn1 = nn.BatchNorm2d(64)
-        self.maxpool = nn.MaxPool2d(3, 2) #64 16 16 -> 64 8 8
 
         self.conv2_1 = BasicBlock(64, 64, 1)
-        self.conv2_2 = BasicBlock(64, 128, 2,
+        self.conv2_2 = BasicBlock(64, 64, 1)
+
+        self.conv3_1 = BasicBlock(64, 128, 2,
                                 downsample=nn.Sequential(
-                                    nn.Conv2d(64, 128, 1, 2),
+                                    nn.Conv2d(64, 128, 1, 2, bias=False),
                                     nn.BatchNorm2d(128)
                                 ))
-        # 64 8 8 -> 128, 4, 4
-        self.conv3_1 = BasicBlock(128, 128, 1)
-        self.conv3_2 = BasicBlock(128, 256, 2,
+        # 64 32 32 -> 128, 16, 16
+        self.conv3_2 = BasicBlock(128, 128, 1)
+        self.conv4_1 = BasicBlock(128, 256, 2,
                                 downsample=nn.Sequential(
-                                    nn.Conv2d(128, 256, 1, 2),
+                                    nn.Conv2d(128, 256, 1, 2, bias=False),
                                     nn.BatchNorm2d(256)
                                 ))
-        # 128, 4, 4 -> 256, 2, 2
-        self.avgpool = nn.AvgPool2d(2,1)
-        self.fc = nn.Linear(256, 10)
+        # 128, 16, 16 -> 256, 8, 8
+        self.conv4_2 = BasicBlock(256, 256, 1)
+
+        self.conv5_1 = BasicBlock(256, 512, 2, 
+                                downsample=nn.Sequential(
+                                    nn.Conv2d(256, 512, 1, 2, bias=False),
+                                    nn.BatchNorm2d(512)
+                                  ))
+        # 256, 8, 8 -> 512, 4, 4
+        self.conv5_2 = BasicBlock(512, 512, 1)
+        #self.conv5_2 = BasicBlock(512, 512, 1)
+        #512,2,2
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+
+        self.fc = nn.Linear(512, 100)
+        #self.fc1 = nn.Linear(512, 100)
+        #self.fc2 = nn.Linear(100, 10)
+
+        self.relu = nn.ReLU(inplace=True)
     def forward(self, x):       
         x = self.conv1(x)
-        x = F.relu(self.bn1(x))
-        x = self.maxpool(x)
+        x = self.relu(self.bn1(x))
+
         x = self.conv2_1(x)
         x= self.conv2_2(x)
+
         x = self.conv3_1(x)
         x = self.conv3_2(x)
+
+        x = self.conv4_1(x)
+        x = self.conv4_2(x)
+
+        x = self.conv5_1(x)
+        x = self.conv5_2(x)
+
         x = self.avgpool(x)
         x = torch.flatten(x, 1) # flatten all dimensions except batch
+        
         x = self.fc(x)
-        x = F.softmax(x)
+        #x = self.relu(self.fc1(x))
+        #x = F.softmax(self.fc2(x), dim = 1)
+        #x = F.softmax(x, dim=1)
         return x
 
 net = ResNet()
 model = net.to(device)
+learn_rate = 0.001
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+criterion = nn.CrossEntropyLoss().to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=learn_rate)
 
 def test(loss_vector, accuracy_vector):
     model.eval()
@@ -128,7 +155,8 @@ def train(epoch):
     for i, data in enumerate(trainloader, 0):
         # get the inputs; data is a list of [inputs, labels]
         inputs, labels = data
-
+        inputs = inputs.to(device)
+        labels = labels.to(device)
         # zero the parameter gradients
         optimizer.zero_grad()
 
@@ -140,17 +168,21 @@ def train(epoch):
 
         # print statistics
         running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+        if i % 25 == 24:    # print every 2000 mini-batches
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 25:.3f}')
             running_loss = 0.0
-
             
 if __name__ == "__main__":
 
     lossv,accv=[],[]
 
     print("Begin training")
-    for epoch in range(5):  # loop over the dataset multiple times
+    for epoch in range(35):  # loop over the dataset multiple times
         train(epoch)
         test(lossv,accv)
     print('Finished Training')
+
+    PATH = '/codes/DeepLearning/DL/CNN/cifar_resnet_35.pth'
+    torch.save(net.state_dict(), PATH)
+
+torchvision.models.resnet18()
---
+++ FFN/MLPMixer.py
@@ -0,0 +1,164 @@
+from pytest import xfail
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+from einops.layers.torch import Rearrange
+import numpy as np
+
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+    
+print('Using PyTorch version:', torch.__version__, ' Device:', device)
+
+batch_size = 32
+
+train_dataset = datasets.MNIST('/codes/DeepLearning/DL/FFN/data', 
+                               train=True, 
+                               download=True, 
+                               transform=transforms.ToTensor())
+
+validation_dataset = datasets.MNIST('/codes/DeepLearning/DL/FFN/data', 
+                                    train=False, 
+                                    transform=transforms.ToTensor())
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, 
+                                           batch_size=batch_size, 
+                                           shuffle=True)
+
+validation_loader = torch.utils.data.DataLoader(dataset=validation_dataset, 
+                                                batch_size=batch_size, 
+                                                shuffle=False)
+
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, hidden_dim):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(0.2),
+            nn.Linear(hidden_dim, dim),
+            nn.Dropout(0.2)
+        )
+    def forward(self, x):
+        return self.net(x)
+
+class MLPMixerBlock(nn.Module):
+    def __init__(self, dim, num_patches, token_dim, channel_dim):
+        super().__init__()
+        self.token_mlp = nn.Sequential(
+            nn.LayerNorm(dim),
+            Rearrange('b n d -> b d n'),
+            FeedForward(num_patches, token_dim),
+            Rearrange('b d n -> b n d')
+        )
+        self.channel_mlp = nn.Sequential(
+            nn.LayerNorm(dim),
+            FeedForward(dim, channel_dim)
+        )
+
+    def forward(self, x):
+        x = x + self.token_mlp(x)
+        x = x + self.channel_mlp(x)
+        return x
+        
+
+class MLPMixer(nn.Module):
+    def __init__(self, image_size, channels, patch_size, dim, depth, token_dim, channel_dim):
+        super(MLPMixer, self).__init__()
+        assert image_size % patch_size == 0
+
+        self.num_patches = (image_size // patch_size) **2
+
+        self.patch_embedding = nn.Sequential(
+            nn.Conv2d(channels, dim, patch_size, patch_size),
+            Rearrange('b c h w -> b (h w) c'),
+        )
+
+        self.mixer_blocks = nn.ModuleList([])
+        for _ in range(depth):
+            self.mixer_blocks.append(MLPMixerBlock(dim, self.num_patches, token_dim, channel_dim))
+        
+        self.ln = nn.LayerNorm(dim)
+
+        self.fc = nn.Linear(dim, 10)
+    
+    def forward(self, x):
+        x = self.patch_embedding(x)
+
+        for block in self.mixer_blocks:
+            x = block(x)
+
+        x = self.ln(x)
+        x = x.mean(dim=1)
+        x = F.log_softmax(self.fc(x), dim=1)
+        return x
+
+model = MLPMixer(image_size=28, channels=1, patch_size=7, dim=32, depth=4, token_dim=64, channel_dim=128).to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+#optimizer = torch.optim.SGD(model.parameters(), lr=0.005, momentum=0.5)
+criterion = nn.CrossEntropyLoss()
+
+def train(epoch, log_interval=200):
+    # Set model to training mode
+    model.train()
+    
+    # Loop over each batch from the training set
+    for batch_idx, (data, target) in enumerate(train_loader):
+        # Copy data to GPU if needed
+        data = data.to(device)
+        target = target.to(device)
+
+        # Zero gradient buffers
+        optimizer.zero_grad() 
+        
+        # Pass data through the network
+        output = model(data)
+
+        # Calculate loss
+        loss = criterion(output, target)
+
+        # Backpropagate
+        loss.backward()  
+        
+        # Update weights
+        optimizer.step()    #  w - alpha * dL / dw
+        
+        if batch_idx % log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.data.item()))
+            
+
+def validate(loss_vector, accuracy_vector):
+    model.eval()
+    val_loss, correct = 0, 0
+    for data, target in validation_loader:
+        data = data.to(device)
+        target = target.to(device)
+        output = model(data)
+        val_loss += criterion(output, target).data.item()
+        pred = output.data.max(1)[1] # get the index of the max log-probability
+        correct += pred.eq(target.data).cpu().sum()
+
+    val_loss /= len(validation_loader)
+    loss_vector.append(val_loss)
+
+    accuracy = 100. * correct.to(torch.float32) / len(validation_loader.dataset)
+    accuracy_vector.append(accuracy)
+    
+    print('\nValidation set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        val_loss, correct, len(validation_loader.dataset), accuracy))
+    
+epochs = 10
+
+if __name__ == "__main__":
+    print(model)
+
+    lossv, accv = [], []
+    for epoch in range(1, epochs + 1):
+        train(epoch)
+        validate(lossv, accv)
